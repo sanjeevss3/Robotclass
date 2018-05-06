6ec402f5a0bde63927c02eafc16fcc98ac865cdb
sanjeevss3 <sanjeevss.chaudhary@gmail.com>
2018-05-06 15:21:14 +0530

    New class added

---
+++ javarobot/src/main/java/com/robot/javarobot/TestClass.java
@@ -0,0 +1,17 @@
+package com.robot.javarobot;
+
+
+/**
+*
+*@author Sanjeev Chaudhary
+*https://github.com/sanjeevss3/
+*
+*/
+
+public class TestClass {
+	public static void main(String[] args)
+	{
+		System.out.println("Git hub push");
+	}
+
+}
