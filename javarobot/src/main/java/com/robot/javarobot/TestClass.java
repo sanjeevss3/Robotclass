6996e6e9dff984dff200e70ef769a318800daf3b
sanjeevss3 <sanjeevss.chaudhary@gmail.com>
2018-05-10 21:06:48 +0530

    updated on May-10-2018

--- javarobot/src/main/java/com/robot/javarobot/TestClass.java
+++ javarobot/src/main/java/com/robot/javarobot/TestClass.java
@@ -1,18 +1,13 @@
 package com.robot.javarobot;
 
-
 /**
-*
-*@author Sanjeev Chaudhary
-*https://github.com/sanjeevss3/
-*
-*/
+ *
+ * @author Sanjeev Chaudhary https://github.com/sanjeevss3/
+ *
+ */
 
 public class TestClass {
-	public static void main(String[] args)
-	{
-		System.out.println("Git hub push");
-		System.out.println("Git hub push");
+	public static void main(String[] args) {
 		System.out.println("Git hub push");
 		System.out.println("Git hub push");
 	}
